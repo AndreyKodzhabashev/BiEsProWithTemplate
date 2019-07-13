3ee3e0d59edfc2e4b0d3217284530f0e5b502086
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-07-13 17:06:01 +0300

    Added PropertyBaseModel:BaseDeletableModel<string>

---
+++ ASP.NET Core/Data/BiEsPro.Data.Common/Models/PropertyBaseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BiEsPro.Data.Common.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public abstract class PropertyBaseModel : BaseDeletableModel<string>
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Code { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+    }
+}
