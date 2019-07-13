097690920a3d82b8a9ca3d38ceb6cd7244005ab2
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-07-13 16:31:11 +0300

    Remastered Login - login with UserName. Settings tab and controller available only for Administrator

--- ASP.NET Core/Web/BiEsPro.Web/Controllers/SettingsController.cs
+++ ASP.NET Core/Web/BiEsPro.Web/Controllers/SettingsController.cs
@@ -4,13 +4,15 @@
     using System.Linq;
     using System.Threading.Tasks;
 
+    using BiEsPro.Common;
     using BiEsPro.Data.Common.Repositories;
     using BiEsPro.Data.Models;
     using BiEsPro.Services.Mapping;
     using BiEsPro.Web.ViewModels.Settings;
-
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     public class SettingsController : BaseController
     {
         private readonly IDeletableEntityRepository<Setting> repository;
